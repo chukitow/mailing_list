ec526c6e36badfbbfce690dfd8c2d9befb0df7cb
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2016-07-18 19:52:11 -0500

    add linkedin data to mailing model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160718004126) do
+ActiveRecord::Schema.define(version: 20160718050542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,9 @@ ActiveRecord::Schema.define(version: 20160718004126) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "linkedin_id"
+    t.integer  "linkedin_connections"
+    t.string   "linkedin_pictureUrl"
   end
 
 end
