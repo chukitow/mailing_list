b7cd8a04231b858527262e6a92b9ffd852456ebd
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2016-07-18 22:11:32 -0500

    build connect with twitter

---
+++ app/controllers/auth/twitter_controller.rb
@@ -0,0 +1,18 @@
+class Auth::TwitterController < ApplicationController
+  def callback
+    if params[:oauth_token].present? && params[:oauth_verifier].present?
+      mailing = Mailing.find(params[:mailing_id])
+
+      mailing.update_attributes({
+        twitter_id: request.env['omniauth.auth']['extra']['raw_info']['id'],
+        twitter_followers: request.env['omniauth.auth']['extra']['raw_info']['followers_count'],
+        twitter_follows: request.env['omniauth.auth']['extra']['raw_info']['friends_count'],
+        twitter_profile_image_url: request.env['omniauth.auth']['extra']['raw_info']['profile_image_url']
+      })
+
+      redirect_to mailing
+    else
+      redirect_to root_url
+    end
+  end
+end
---
+++ spec/controllers/auth/twitter_controller_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+describe Auth::TwitterController, 'GET#callback' do
+  context 'when oauth_token and oauth_token are present in the url' do
+    let(:mailing) { FactoryGirl.create(:mailing) }
+
+    before do
+      controller.request.env['omniauth.auth'] = {
+        'extra' => {
+          'raw_info' => {
+            'id' => Faker::Number.number(3),
+            'followers_count' => Faker::Number.number(3),
+            'friends_count' => Faker::Number.number(3),
+            'profile_image_url' => Faker::Placeholdit.image,
+          }
+        }
+      }
+
+      get :callback, mailing_id: mailing.id, oauth_token: Faker::Lorem.characters, oauth_verifier: Faker::Lorem.characters
+    end
+
+    it 'updates mailing twitter information' do
+      expect(mailing.reload.twitter_id).to_not be_nil
+      expect(mailing.reload.twitter_followers).to_not be_nil
+      expect(mailing.reload.twitter_follows).to_not be_nil
+      expect(mailing.reload.twitter_profile_image_url).to_not be_nil
+    end
+
+    it 'redirects to mailing' do
+      expect(response).to redirect_to(mailing)
+    end
+  end
+
+  context 'when oauth_token and oauth_token are not present in the url' do
+    it 'redirects to root url' do
+      get :callback
+
+      expect(response).to redirect_to(root_url)
+    end
+  end
+end
