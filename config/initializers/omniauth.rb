ae024283511c24c3e34cdb26ebb864742e1d3d64
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2016-07-19 17:30:27 -0500

    add omniauth facebook dependencies

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['twitter_consumer_key'], ENV['twitter_consumer_secret']
+  provider :facebook, ENV['facebook_key'], ENV['facebook_secret'], scope: 'user_friends'
 end
